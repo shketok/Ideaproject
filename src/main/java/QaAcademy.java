32ab7102e0833e695b29ef8102f76244780c4955
artem <a.lebakov@a1qa.com>
2020-04-28 20:36:59 +0300

    addded numbers

--- src/main/java/QaAcademy.java
+++ src/main/java/QaAcademy.java
@@ -1,5 +1,5 @@
 public class QaAcademy {
     public static void main(String[] args) {
-        System.out.println("hello world12312312312312312312");
+        System.out.println("hello world12312312312312312312111111111111111111111111111");
     }
 }
